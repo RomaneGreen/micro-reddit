b86794da3bb05598206a8a208823e338f7239001
Romane <romane71193@gmail.com>
2018-06-30 13:19:33 -0400

    longer length validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates_length_of :body, :minimum => 5
-    has_many :comments 
+  validates_length_of :body, :minimum => 7
+    has_many :comments
 end
